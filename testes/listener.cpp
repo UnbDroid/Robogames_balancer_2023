2e1862d1d493299e25ee73f5db147d5cb4975847
Thamires de Pontes <thamiresdepontesdasilva@gmail.com>
2023-06-03 11:01:01 -0300

    Solucao final da competição

--- testes/listener.cpp
+++ testes/listener.cpp
@@ -15,6 +15,7 @@
 #include "std_msgs/Float32.h"
 #include "std_msgs/Float64.h"
 #include "std_msgs/Float32MultiArray.h"
+#include <sched.h>
 
 // Defines ------------------------------------------------------------------------------------------------------------
 
@@ -75,15 +76,20 @@ int saturadoDireito = 0;
 int saturadoEsquerdo = 0;
 
 float theta = 0;
+float alfa = 0;
 float referencia = 0;
 float velocidade = 0;
 double posicao = 0;
 float omega = 0;
+float alfa0 = 0;
 
 float referenceTheta = 0;
 float referenceVelocidade = 0;
 float referencePosicao = 0;
 float referenceOmega = 0;
+
+float voltas_esquerda_old;
+float voltas_direita_old;
 
 // Cálculos
 float comprimento_roda = 2 * PI * RAIO_RODA;
@@ -96,14 +102,19 @@ float comprimento_roda = 2 * PI * RAIO_RODA;
 #define QUEUE_SIZE 5000
 #define MEDIA_MODEL_SIZE 50
 #define TB_ROLL_Y 1
+#define TB_YAW_Z 2
 #define NUM_PARAMS 4
 #define MAX_ERRO_POS 0.3f
+#define DELTA_REFERENCIA 0.25f
 
 static rc_mpu_data_t mpu_data;
 
 static float prev_angle = 0.0;
 static ros::Time prev_time;
 
+float uDir = 0;
+float kpDir = 2.0;
+float u[6] = {0};
 // Declração de funções -----------------------------------------------------------------------------------------------------
 
 // Funções dos motores
@@ -240,6 +251,8 @@ static void dmp_callback(void)
     {
         theta = (mpu_data.dmp_TaitBryan[TB_ROLL_Y]) + 0.05;
 
+        alfa = mpu_data.dmp_TaitBryan[TB_YAW_Z];
+
         if (prev_time.isZero())
         {
             prev_time = current_time_imu;
@@ -299,6 +312,14 @@ int main(int argc, char *argv[])
 
     // ROS ------------------------------------------------------------------------------
 
+    // struct sched_param sched_param;
+    // sched_param.sched_priority = 50;
+
+    // if(sched_setscheduler(0, SCHED_FIFO, &sched_param) == -1){
+    //     ROS_ERROR("Falha ao definir o escalonamento");
+    //     return -1;
+    // }
+
     // Inicializa e cria o nó
     ros::init(argc, argv, "controle_velocidade");
     ros::NodeHandle n;
@@ -349,7 +370,8 @@ int main(int argc, char *argv[])
 
     // Funcionou um metro
     // float K[6] = {15.5, 0.2690, 3.5, 0.85, 0, 0.000081};
-    float K[6] = {15.5, 0.2690, 3.5, 0.85, 0, 0.000081};
+    float K[6] = {15.6, 0.2690, 4.2, 0.85, 0, 0.000082};
+    // float K[6] = {14.7, 0.2690, 4.5, 0.85, 0, 0.000081};
     // float K[6] = {16.5, 0.2690, 3.8, 0.81, 0, 0.000081};
     // float K[6] = {15, 0.2690, 2.1, 0.85, 0, 0.00008};
 
@@ -360,8 +382,6 @@ int main(int argc, char *argv[])
     float aceleracao = 0;
     float velocidade_old = 0;
     float referencePosicaoTeto = 0;
-
-    static ros::Time last_publish_time = ros::Time::now();
 
     int count = 0;
     double total_period = 0.0;
@@ -381,21 +401,40 @@ int main(int argc, char *argv[])
         rc_usleep(1);
     }
 
-    printf("%f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f \n", K[0], K[1], K[2], K[3], K[4], K[5], 0, 0, 0, 0, 0, 0);
+    alfa0 = alfa;
+
+    printf("%f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f \n", K[0], K[1], K[2], K[3], K[4], K[5], 0, 0, 0, 0, 0, 0, 0, referencePosicao, referenceVelocidade,0, 0);
 
     while (ros::ok())
     {
 
+        static ros::Time last_publish_time = ros::Time::now();
         ros::Time current_time = ros::Time::now();
 
         ros::Duration dt = current_time - last_publish_time;
 
-        count++;
-        total_period += dt.toSec();
+        float dt_sec = (dt).toSec();
 
-        if ((dt).toSec() >= 1.0 / PUBLISH_RATE_HZ)
+        // if(dt_sec >= 0.003){
+        //     dt_sec = 0.003;
+        // }
+
+        // count++;
+        // total_period += dt.toSec();
+
+        if (dt_sec != 0)
         {
-            referencePosicao += referenceVelocidade * (dt).toSec();
+
+            if (voltas_esquerda_old != voltas_esquerda || voltas_direita_old != voltas_direita)
+            {
+                referencePosicao += referenceVelocidade * dt_sec;
+                if(referencePosicao < (posicao - DELTA_REFERENCIA) && referenceVelocidade != 0){
+                    referencePosicao = posicao - DELTA_REFERENCIA;
+                }
+            }
+
+                // referencePosicao += referenceVelocidade * dt_sec;
+
 
             // referencePosicaoTeto += referenceVelocidade *(dt).toSec();
             // referencePosicao = posicao + 0.05;
@@ -404,7 +443,7 @@ int main(int argc, char *argv[])
             //     referencePosicao = referencePosicaoTeto;
             // }
 
-            if (referencePosicao >= 4.0)
+            if (referencePosicao >= 7.5)
             {
                 referenceVelocidade = 0;
             }
@@ -428,14 +467,17 @@ int main(int argc, char *argv[])
             voltas_esquerda_anterior = voltas_esquerda;
             voltas_direita_anterior = voltas_direita;
 
+            voltas_esquerda_old = voltas_esquerda;
+            voltas_direita_old = voltas_direita;
+
             voltas_esquerda = -encoder0Pos / (double)4096;
             voltas_direita = encoder1Pos / (double)4096;
 
             // velocidade_esquerda_old = velocidade_esquerda;
             // velocidade_direita_old = velocidade_direita;
 
-            velocidade_esquerda[count_velocidade] = (voltasParaMetros(voltas_esquerda - voltas_esquerda_anterior)) / dt.toSec();
-            velocidade_direita[count_velocidade] = (voltasParaMetros(voltas_direita - voltas_direita_anterior)) / dt.toSec();
+            velocidade_esquerda[count_velocidade] = (voltasParaMetros(voltas_esquerda - voltas_esquerda_anterior)) / dt_sec;
+            velocidade_direita[count_velocidade] = (voltasParaMetros(voltas_direita - voltas_direita_anterior)) / dt_sec;
 
             velocidade_direita_media += velocidade_direita[count_velocidade] / MEDIA_MODEL_SIZE;
             velocidade_esquerda_media += velocidade_esquerda[count_velocidade] / MEDIA_MODEL_SIZE;
@@ -465,9 +507,9 @@ int main(int argc, char *argv[])
             velocidade_old = velocidade;
             velocidade = velocidade_media;
 
-            iPosicao += posicao * (dt).toSec();
+            iPosicao += posicao * dt_sec;
 
-            aceleracao = (velocidade - velocidade_old) / (dt).toSec();
+            aceleracao = (velocidade - velocidade_old) / dt_sec;
 
             error[0] = theta - referenceTheta;
             error[1] = omega - referenceOmega;
@@ -479,32 +521,40 @@ int main(int argc, char *argv[])
             // printf("%f, %f, %f, %f \n", theta, omega, posicao, velocidade);
 
             // Controlador
-            referencia = 0;
 
             for (int i = 0; i < 6; i++)
             {
-                if (i != 0)
+                u[i] = error[i] * K[i];
+
+                if ((i != 0) && (i != 2))
                 {
-                    if (error[i] * K[i] >= MAX_ERRO_POS)
+                    if (u[i] >= MAX_ERRO_POS)
                     {
-                        referencia += MAX_ERRO_POS;
+                        u[i] = MAX_ERRO_POS;
                     }
                     else
                     {
-                        if (error[i] * K[i] <= -MAX_ERRO_POS)
+                        if (u[i] <= -MAX_ERRO_POS)
                         {
-                            referencia -= MAX_ERRO_POS;
+                            u[i] = -MAX_ERRO_POS;
                         }
                         else
                         {
-                            referencia += error[i] * K[i];
+                            u[i] = error[i] * K[i];
                         }
                     }
                 }
                 else
                 {
-                    referencia += error[i] * K[i];
+                    u[i] = error[i] * K[i];
                 }
+            }
+
+            referencia = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                referencia += u[i];
             }
 
             // if (theta < 0)
@@ -516,16 +566,16 @@ int main(int argc, char *argv[])
             //     velocidade_referencia = 1.8;
             // }
 
-            float thetaReferencia = error[0] * K[0];
-            float omegaReferencia = error[1] * K[1];
-            float posicaoReferencia = error[2] * K[2];
-            float velocidadeReferencia = error[3] * K[3];
-            float iPosicaoReferencia = error[4] * K[4];
-            float aceleracaoReferencia = error[5] * K[5];
+            float thetaReferencia = u[0];
+            float omegaReferencia = u[1];
+            float posicaoReferencia = u[2];
+            float velocidadeReferencia = u[3];
+            float iPosicaoReferencia = u[4];
+            float aceleracaoReferencia = u[5];
 
             velocidade_referencia = referencia;
             // printf("%f, %f, %f, %f\n", dt.toSec(), velocidade_direita_media, velocidade_esquerda_media, velocidade_referencia);
-            printf("%f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f\n", thetaReferencia, omegaReferencia, posicaoReferencia, velocidadeReferencia, aceleracaoReferencia, theta, omega, posicao, velocidade, aceleracao, referencePosicao, referenceVelocidade);
+            printf("%f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f\n", thetaReferencia, omegaReferencia, posicaoReferencia, velocidadeReferencia, aceleracaoReferencia, theta, omega, posicao, velocidade, aceleracao, referencePosicao, referenceVelocidade, ros::Time::now().toSec(), voltas_direita, voltas_esquerda, dt_sec, alfa);
             // printf("%f, %f, %f, %f\n", theta, omega, velocidade, posicao);
             // printf("%f\n", theta);
             // printf("%f, %f\n", theta, omega);
@@ -546,12 +596,15 @@ int main(int argc, char *argv[])
                 somatorio_erro_direita += erro_direita;
             }
 
-            potencia_motor_esquerda = erro_esquerda * Kp_esquerda + (somatorio_erro_esquerda * Ki_esquerda) * dt.toSec();
-            potencia_motor_direita = erro_direita * Kp_direita + (somatorio_erro_direita * Ki_direita) * dt.toSec();
+            potencia_motor_esquerda = erro_esquerda * Kp_esquerda + (somatorio_erro_esquerda * Ki_esquerda) * dt_sec;
+            potencia_motor_direita = erro_direita * Kp_direita + (somatorio_erro_direita * Ki_direita) * dt_sec;
+
+            uDir = kpDir * (alfa - alfa0);
+            // uDir = kpDir * (voltasParaMetros(voltas_direita - voltas_esquerda));
 
             // Output
-            motorEsquerda(potencia_motor_esquerda);
-            motorDireita(potencia_motor_direita);
+            motorEsquerda(potencia_motor_esquerda + uDir);
+            motorDireita(potencia_motor_direita - uDir);
 
             // printf("%f, %f\n", velocidade, referencia);
             // printf("%f\n", velocidade);
